7b209e4fd46ca149663e67f774b9b68c8ae72ed1
MarcelaTriana <marcelatrianamogollon@gmail.com>
2021-02-04 02:16:00 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Diana Triana
+ */
+
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: Always 0 (Success)
+ * Return: Always 1.
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+  write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
+
+  return (1);
 }
